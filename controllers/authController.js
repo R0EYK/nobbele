48a9bdb9d1e37996550aa7f7af52cdbec8f6a248
R0EYK <roeyk70@gmail.com>
2024-07-07 19:39:54 +0300

    Roey 1.06
    Login now working , unlike when Amit try to do it

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,18 @@
-// controllers/authController.js
 const User = require('../models/userModel');
+const session = require('express-session');
+
 
 exports.getLogin = (req, res) => {
   res.render('login');
+};
+
+exports.logout = (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).send('Error logging out: ' + err.message);
+    }
+    res.redirect('/login'); // Redirect to login page after logout
+  });
 };
 
 exports.postLogin = async (req, res) => {
@@ -20,8 +30,23 @@ exports.postLogin = async (req, res) => {
       return res.status(400).send('Invalid username or password');
     }
 
+    // Ensure req.session is defined before setting properties
+    if (!req.session) {
+      throw new Error('Session not initialized');
+    }
+
+    // Set session variables
+    req.session.userId = user._id;
+    req.session.username = user.username;
+    req.session.loggedIn = true;
+
     res.send('User logged in successfully!');
   } catch (error) {
     res.status(500).send('Error logging in: ' + error.message);
   }
+};
+
+exports.checkLoggedIn = (req, res, next) => {
+  res.locals.loggedIn = req.session.loggedIn || false;
+  next();
 };
