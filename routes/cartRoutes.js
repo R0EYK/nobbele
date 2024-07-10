4465b7f028034004814abd909da98c006c5a3595
AmitMichael12 <amit.michael100@gmail.com>
2024-07-10 13:05:56 +0300

    Amit2.07
    add option to remove from cart

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Cart = require('../models/cartModel');
 const Product = require('../models/productModel');
-const User = require('../models/userModel');
 
 // פונקציה להוספת מוצר לסל
 router.post('/add-to-cart', async (req, res) => {
@@ -51,6 +50,41 @@ router.get('/cart', async (req, res) => {
   } catch (error) {
     console.error('Failed to retrieve cart', error);
     res.status(500).send('Failed to retrieve cart');
+  }
+});
+
+// פונקציה למחיקת מוצר מהסל
+router.post('/remove-from-cart', async (req, res) => {
+  const userId = req.session.userId;
+  const { productId } = req.body;
+
+  try {
+    const cart = await Cart.findOne({ userId });
+
+    if (!cart) {
+      return res.status(404).json({ message: 'Cart not found' });
+    }
+
+    const productIndex = cart.products.findIndex(p => p.productId.toString() === productId);
+
+    if (productIndex === -1) {
+      return res.status(404).json({ message: 'Product not found in cart' });
+    }
+
+    const product = await Product.findById(productId);
+    const productPrice = product.price * cart.products[productIndex].quantity;
+
+    // הסרת המוצר מהעגלה
+    cart.products.splice(productIndex, 1);
+
+    // עדכון המחיר הכולל של העגלה
+    cart.totalPrice -= productPrice;
+
+    await cart.save();
+    res.status(200).json({ message: 'Product removed from cart successfully' });
+  } catch (error) {
+    console.error('Failed to remove product from cart', error);
+    res.status(500).json({ message: 'Failed to remove product from cart', error });
   }
 });
 
