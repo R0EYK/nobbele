9b19a1b3f79b7032032009773cd5bb1f942d5b34
R0EYK <roeyk70@gmail.com>
2024-07-17 16:43:20 +0300

    UI & Change Password Feature

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,5 +13,10 @@ router.post('/signup', userController.postSignup);
 // GET /user/orders - Display all user orders
 router.get('/orders', authController.isAuthenticated , userController.getUserOrders);
 
+//changePassword page
+router.get('/changePassword', authController.isAuthenticated, userController.changePasswordPage);
+router.post('/changePassword', authController.isAuthenticated, userController.changePassword);
+
+
 
 module.exports = router;
